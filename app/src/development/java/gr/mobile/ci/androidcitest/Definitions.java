96911bc049116549271a7d50c18be83274a6af48
Zisis  Dimitriadis <zisis.dimitriadis@atcom.gr>
2018-12-10 16:48:45 +0200

    add flavors in app

---
+++ app/src/development/java/gr/mobile/ci/androidcitest/Definitions.java
@@ -0,0 +1,8 @@
+package gr.mobile.ci.androidcitest;
+
+public class Definitions {
+
+    public final static String WEB_URL_DEVELOPMENT                    = "www.google.gr";
+
+    public final static String WEB_URL_PRODUCTION                = "www.atcom.gr";
+}
